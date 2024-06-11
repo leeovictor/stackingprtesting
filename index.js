82befc7778ed5a8a44b05f47f2c3eb6b7e049cdf
Leonardo Victor <leonardovictormarques@gmail.com>
2024-06-10 21:55:03 -0300

    configure logger

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 import { printInfo } from 'print.js';
+import { createLogger } from 'logger.js';
 
+const options = {};
 const user = { name: "leonardo", age: 30 };
+const logger = createLogger(options);
+
+logger('starting...');
 printInfo(user);
+logger('ending...');
