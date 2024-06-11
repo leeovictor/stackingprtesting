91c2039843fc6c8d541bde7af3abb3d53516197b
Leonardo Victor <leonardovictormarques@gmail.com>
2024-06-10 22:32:21 -0300

    client adjust

--- index.js
+++ index.js
@@ -1,10 +1,4 @@
 import { printInfo } from 'print.js';
-import { createLogger } from 'logger.js';
-
-const options = {};
 const user = { name: "leonardo", age: 30 };
-const logger = createLogger(options);
 
-logger('starting...');
 printInfo(user);
-logger('ending...');
